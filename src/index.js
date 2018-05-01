2406f4a161acbb1ccd8c487e0474cafdaa61c423
Ashwin Vaswani <ashwinv11@gmail.com>
2018-04-30 23:08:44 -0700

    add aditi guha slide, better contrast for text

--- src/index.js
+++ src/index.js
@@ -4,11 +4,24 @@ import '../node_modules/reveal.js/css/theme/white.css';
 import './style.scss';
 
 Reveal.initialize({
+  center: false,
   controls: false,
   controlsTutorial: false,
   progress: false,
   help: false,
   autoPlayMedia: true,
   mouseWheel: true,
-  transition: 'fade'
+  transition: 'slide'
+});
+
+Reveal.addEventListener('fragmentshown', function(event) {
+  // event.fragment = the fragment DOM element
+  let bgEl = document.getElementsByClassName('slide-background present')[1];
+  bgEl.classList.add('text-focus');
+});
+
+Reveal.addEventListener('fragmenthidden', function(event) {
+  // event.fragment = the fragment DOM element
+  let bgEl = document.getElementsByClassName('slide-background present')[1];
+  bgEl.classList.remove('text-focus');
 });
